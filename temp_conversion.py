cedac1fcca0835eaac5b45cc60d6d51e537434c3
jhiltonjones <124688978+jhiltonjones@users.noreply.github.com>
2024-01-31 15:48:18 +0000

    Update temp_conversion.py

--- temp_conversion.py
+++ temp_conversion.py
@@ -7,12 +7,18 @@ def convert_fahr_to_celsius(fahr):
     """
     
     celsius = (fahr - 32) * (5 / 9)
+    if celsius < -273.15:
+        # If temperture is below absolute zero, throw an error
+        raise ValueError(
+            f"Trying to convert impossible temperture: {fahr}f"
+        )
     return celsius
 
 def convery_fahr_to_kelvin(fahr):
     """
     Arguements
-        fahr:Temperature in fahrenheit
+        :param fahr:Temperature in fahrenheit
+        :return: The temperture in kelvin
     """
     celsius = convert_fahr_to_celsius(fahr)
     kelvin = celsius + 273.15
